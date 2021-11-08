12db5b51ad401855f8ad0521154b311c79217188
Nelson <nelsonclage@gmail.com>
2021-11-08 16:13:02 +0100

    Update answer

--- SQL/Lab_SQL_GroupBy.sql
+++ SQL/Lab_SQL_GroupBy.sql
@@ -1,7 +1,6 @@
 -- 1. In the table actor, what last names are not repeated? For example if you would sort the data in the table actor by last_name, you would 
 -- see that there is Christian Arkoyd, Kirsten Arkoyd, and Debbie Arkoyd. These three actors have the same last name. So we do not want to include this last
 -- name in our output. Last name "Astaire" is present only one time with actor "Angelina Astaire", hence we would want this in our output list.
-
 SELECT last_name AS unique_last_name
 FROM actor
 GROUP BY last_name
